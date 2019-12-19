6c998f74021673fccbe8894db02444e3b84427e6
jnole <jorge.nole@cabsa.es>
2019-12-19 11:55:36 +0100

    Adding ant zorro library and creating layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { ArtistComponent } from './components/artist/artist.component';
-import { SearchComponent } from './components/search/search.component';
-import { SharedComponent } from './components/shared/shared.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {ArtistComponent} from './components/artist/artist.component';
+import {SearchComponent} from './components/search/search.component';
+import {SharedComponent} from './components/shared/shared.component';
+import {RouterModule} from '@angular/router';
+import {ROUTES} from './app.routes';
+import {NavbarComponent} from './components/shared/navbar/navbar.component';
+import {NgZorroAntdModule, NZ_ICONS} from 'ng-zorro-antd';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+import {LayoutComponent} from './components/home/layout/layout.component';
+import {BreadcrumbComponent} from './components/shared/breadcrumb/breadcrumb.component';
+import {FooterComponent} from './components/shared/footer/footer.component';
+import { IconDefinition } from '@ant-design/icons-angular';
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
 
 @NgModule({
   declarations: [
@@ -13,12 +27,19 @@ import { SharedComponent } from './components/shared/shared.component';
     HomeComponent,
     ArtistComponent,
     SearchComponent,
-    SharedComponent
+    SharedComponent,
+    NavbarComponent,
+    LayoutComponent,
+    BreadcrumbComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgZorroAntdModule,
+    RouterModule.forRoot(ROUTES, {useHash: true})
   ],
-  providers: [],
+  providers: [{provide: NZ_ICONS, useValue: icons}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
